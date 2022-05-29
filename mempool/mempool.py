a5bc9c3d18a9ac286f748a5d3f8279a0d1f02979
Stolpmann <evan.stolpmann@hotmail.com>
2022-05-28 21:17:14 -0400

    added sats per byte to mempool scraper

--- mempool/mempool.py
+++ mempool/mempool.py
@@ -16,7 +16,7 @@ ssh.connect(host, port, username, password)
 
 stdin, stdout, stderr = ssh.exec_command(command)
 lines = stdout.readlines()
-
+print("\n")
 
 # prompts user for TXID and then loops through to check mempool
 
@@ -36,7 +36,7 @@ while len(txid) < 64:
 x = find_transaction(txid)
 x = str(x)
 x = x[1:-2]
-print(x)
+print("\n")
 
 command2 = f"docker exec bitcoin bitcoin-cli getrawtransaction {x} true"
 
@@ -56,6 +56,60 @@ def equal(x):
         return False
 
 print(equal(x))
+
+command3 = "docker exec bitcoin bitcoin-cli getrawmempool true"
+
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(host, port, username, password)
+
+
+stdin, stdout, stderr = ssh.exec_command(command3)
+lines = stdout.readlines()
+#print(lines)
+#print(type(lines))
+
+# Get user input of transaction
+
+# Parses through mempool returning index of txid in mempool list
+def mempool_parser(x):
+    y = 0
+    for z in lines:
+        if x in z:
+            return y
+        y += 1
+
+
+y = mempool_parser(x)
+#print(mempool_parser(x))
+
+
+# Slice tx data from mempool list
+tx_data = lines[y:y+27]
+#print(tx_data)
+
+# Extract vsize
+vsize = tx_data[7]
+vsize = vsize[13:-2]
+vsize = int(vsize)
+
+# Extract fee
+fee = tx_data[9]
+fee = fee[11:-2]
+fee = float(fee)
+fee = fee * 100000000
+
+# Fee rate (sats/vbyte)
+sats_per_vbyte = fee/vsize
+
+
+# Print Results
+print("vsize = " + str(vsize))
+print("fees = " + format(fee,'.00000000f'))
+print("sats/vbyte = " + format(sats_per_vbyte, 'f'))
+print("\n")
+
 
 # print("Your TXID is" + x)
 
--- mempool/tx_fee.py
+++ mempool/tx_fee.py
@@ -1,6 +1,7 @@
 import paramiko
 import secrets
 
+
 host = secrets.host
 port = secrets.port
 username = secrets.username
@@ -19,11 +20,12 @@ lines = stdout.readlines()
 print(lines)
 print(type(lines))
 
+# Get user input of transaction
 txid = input("Enter TXID: ")
 
 print(txid)
 
-
+# Parses through mempool returning index of txid in mempool list
 def mempool_parser(txid):
     y = 0
     for x in lines:
@@ -36,20 +38,26 @@ y = mempool_parser(txid)
 print(mempool_parser(txid))
 
 
+# Slice tx data from mempool list
 tx_data = lines[y:y+27]
-
 print(tx_data)
+
+# Extract vsize
 vsize = tx_data[7]
 vsize = vsize[13:-2]
 vsize = int(vsize)
 
+# Extract fee
 fee = tx_data[9]
 fee = fee[11:-2]
 fee = float(fee)
 fee = fee * 100000000
 
+# Fee rate (sats/vbyte)
 sats_per_vbyte = fee/vsize
 
+
+# Print Results
 print(vsize)
 print(format(fee,'.00000000f'))
 print(format(sats_per_vbyte, 'f'))
