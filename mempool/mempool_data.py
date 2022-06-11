cec745c3ebf6a42b1cca31ac1c96801aa21cee9a
Stolpmann <evan.stolpmann@hotmail.com>
2022-06-10 22:13:37 -0400

    mempool frustrations

--- mempool/mempool_data.py
+++ mempool/mempool_data.py
@@ -8,7 +8,9 @@ port = secrets.port
 username = secrets.username
 password = secrets.password
 
-command = "docker exec bitcoin bitcoin-cli getrawmempool true"
+
+
+command = f"docker exec bitcoin bitcoin-cli getrawmempool true"
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -17,40 +19,35 @@ ssh.connect(host, port, username, password)
 stdin, stdout, stderr = ssh.exec_command(command)
 lines = stdout.readlines()
 
-print(lines)
 
 df = pd.DataFrame(lines)
 
+
 df.to_csv('mempool/memdata.csv')
-length = len(lines)
-print(np.array_split(df, ))
 
-
-
-# transactionid = []
-# vsize = []
-# weight = []
-# fee = []
-# modifiedfee = []
-# time = []
-# height = []
-# descendantcount = []
-# descendantsize = []
-# descendantfees = []
-# ancestorcount = []
-# ancestorsize = []
-# ancestorfees = []
-# wtxid = []
-# fees = []
-# base = []
-# modified = []
-# ancestor = []
-# descendant = []
-# depends = []
-# spentby = []
-# bip125_replaceable = []
-# unbroadcast = []
-
-
+# print(len(lines))
+#
+# # length = lines[:27]
+# #
+# # length2 = lines[27:54]
+#
+# def mem_scraper():
+#     mempool = []
+#     for z in lines:
+#         if len(lines) < 27:
+#             return mempool
+#         length = lines[:27]
+#         mempool.append(length)
+#         del lines[:27]
+#     return mempool
+#
+#
+#
+# data = mem_scraper()
+#
+# df = pd.DataFrame(data)
+#
+#
+# df.to_csv('mempool/memdata.csv')
 
 del stdin
